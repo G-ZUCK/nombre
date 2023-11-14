7c99119a1e73c26a2af4fa1c3aac7549601c1054
PedroLic111 <pedro.licapa111@gmail.com>
2023-11-14 18:08:19 -0500

    nombrea de examen

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import C06componente from './component/C06componente';
+import C05contador from './component/C05contador';
+import C04doblecontador from './component/C04doblecontador';
+import C03variable from './component/C03variable';
+import C02operador from './component/C02operador';
+import C01matriz from './component/C01matriz';
+
+function App() {
+  return (
+    <div>
+      -INSTITUTO:
+      <h3>HONORIO DELGADO ESPINOZA"</h3>
+      -NOMBRE:
+      <h3>Tarifa Huillca Julio Cesar</h3>
+      -TURNO:
+      <h3>VI  "SEXTO" "NOCHE"</h3>
+      
+      
+
+
+    </div>
+  );
+}
+
+export default App;
